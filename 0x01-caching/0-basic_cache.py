5fdf5665684938e8e5b8c7f4d4083878ac601f1d
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-08 15:46:31 +0000

    task0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,13 +3,17 @@
 BaseCaching and is a caching system"""
 
 
+BaseCaching = __import__('base_caching').BaseCaching
+
+
 class BasicCache(BaseCaching):
     """caching system doesn’t have limit"""
-    """Add an item in the cache"""
+
     def put(self, key, item):
+        """Add an item in the cache"""
         if key is not None and item is not None:
             self.cache_data[key] = item
 
-    """return the value in self.cache_data linked to key"""
     def get(self, key):
+        """return the value in self.cache_data linked to key"""
          return self.cache_data.get(key, None)
