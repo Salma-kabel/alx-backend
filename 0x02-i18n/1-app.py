2df4eb1dce14b22dcf1be53b119b3c9589c72b8a
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-14 13:09:55 +0000

    task1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """instantiate the Babel object in your app"""
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -14,7 +14,7 @@ class Config:
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    app.config.from_object(Config)
+app.config.from_object(Config)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
