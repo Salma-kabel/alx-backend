e9fe4656c9305f7f1a137918ebb231f59046f004
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-14 13:05:27 +0000

    task0

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""instantiate the Babel object in your app"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """ configure available languages in our app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+    app.config.from_object(Config)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """outputs “Welcome to Holberton” as page title"""
+    return render_template('1-index.html')
