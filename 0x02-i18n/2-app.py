3de017abfc1c6b1d1b8b289029b34c8693d2f49b
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-15 13:11:16 +0000

    task3

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """outputs “Welcome to Holberton” as page title"""
-    return render_template('2-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
