79812abfa6d3a151fb665c46272927e5b2015929
Salma_Salah <salma.s.kabel@gmail.com>
2024-05-15 13:06:50 +0000

    task3

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Create a get_locale function with the
+babel.localeselector decorator"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, negotiate_locale
+
+
+class Config:
+    """ configure available languages in our app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """outputs “Welcome to Holberton” as page title"""
+    return render_template('2-index.html')
