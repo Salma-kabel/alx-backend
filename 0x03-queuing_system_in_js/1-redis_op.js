fbd39924848aba546fda3f112d8271c606fbbdca
Salma_Salah <salma.s.kabel@gmail.com>
2024-06-10 16:14:11 +0000

    task1

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+const client = redis.createClient();
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+}).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, (err, reply) => {
+        if (err) {
+            console.error('Error setting value:', err);
+        } else {
+            console.log('Reply:', reply);
+        }
+    });
+}
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, (err, value) => {
+        if (err) {
+            console.error('error');
+        } else {
+            console.log(value);
+        }
+    })
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
