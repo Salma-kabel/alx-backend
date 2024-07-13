e88e198cb3292ed9595e63e59e768faa1edc3d12
Salma_Salah <salma.s.kabel@gmail.com>
2024-07-13 01:51:00 +0000

    task1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -4,3 +4,30 @@ BaseCaching and is a caching system"""
 
 
 BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """class that is a caching system"""
+    def __init__(self):
+        """initiate method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value for the key"""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key = list(self.cache_data.keys())[0]
+            del self.cache_data[discarded_key]
+
+            print("DISCARD: {}".format(discarded_key))
+
+    def get(self, key):
+        """returns the value in self.cache_data linked to key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
